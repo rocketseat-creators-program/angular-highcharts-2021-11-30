51de23cd0665c873810f2a160e4be8d05df41fd2
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-11-30 01:07:50 -0300

    Default structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/snacks"
+  }, {
+    path: "snacks",
+    loadChildren: () => import('./feature/snacks/snacks.module').then(m => m.SnacksModule)
+  }, {
+    path: "**",
+    redirectTo: "/snacks"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
